edf3ed5bb5cbef96a86d9e0d164673ae709154dd
Wayne <yky32is@gmail.com>
2023-04-09 03:44:40 +0800

    2bd

---
+++ src/app/login/page.tsx
@@ -0,0 +1,21 @@
+'use client'
+
+import { FC } from 'react'
+
+interface pageProps {
+  
+}
+
+const page: FC<pageProps> = ({}) => {
+  return <>
+    <div className='flex min-h-full items-center justify-center py-12 px-4 sm:px-6 lg:px-8'>
+      <div className='w-full flex flex-col items-center max-w-md space-y-8'>
+        <div className='flex flex-col items-center'>
+
+        </div>
+      </div>
+    </div>
+  </>
+}
+
+export default page
