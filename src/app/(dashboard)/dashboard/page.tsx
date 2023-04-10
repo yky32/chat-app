990d15007aedf942d52acbe3e8205fd18c091e9f
Wayne <yky32is@gmail.com>
2023-04-11 02:19:37 +0800

    Add signout button and side nav

--- src/app/(dashboard)/dashboard/page.tsx
+++ src/app/(dashboard)/dashboard/page.tsx
@@ -3,10 +3,9 @@ import { authOptions } from '@/lib/auth'
 import { getServerSession } from 'next-auth'
 import { FC } from 'react'
 
-
 const page = async ({ }) => {
-  const session = await getServerSession(authOptions)
-  return <>{JSON.stringify(session)}</>
+  
+  return <>this is dashboard index</>
 }
 
 export default page
